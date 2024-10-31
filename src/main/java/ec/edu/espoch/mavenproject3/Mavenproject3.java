88897bcb77778ff5de270726158159374f35c8e9
Jordy-Segura <marcelo2005jmsp@gmail.com>
2024-10-31 17:41:22 -0500

    puerto 435

---
+++ src/main/java/ec/edu/espoch/mavenproject3/Mavenproject3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.mavenproject3;
+
+/**
+ *
+ * @author jordy
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
